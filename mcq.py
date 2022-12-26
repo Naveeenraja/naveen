513f8b1e45ac7e6cb1f22728394df6d758104b4f
Naveeenraja <120572069+Naveeenraja@users.noreply.github.com>
2022-12-26 01:27:14 -0800

    Add files via upload

--- mcq.py
+++ mcq.py
@@ -1,16 +1,14 @@
 from flask import Flask,render_template,request,redirect
 
 import data.mcqutil as mcq_utils
-file ="studreg.json"
-file1 = "mcq.json"
+file ="mcq question\studreg.json"
+file1 = "mcq question\mcq.json"
 app=Flask(__name__)
 
 @app.route("/", methods=["POST" , "GET"])
 
 def index():
-    data = mcq_utils.read_json(file)
-    return render_template("login.html", data= data["student registration"] )
-
+    return render_template("login.html")
 @app.route("/login", methods= ["POST","GET"])
 def login():
     data = mcq_utils.read_json(file)
@@ -18,7 +16,7 @@ def login():
 
 @app.route("/register", methods=["POST" , "GET"])
 def reg():
-    msg="Your registration has been completed successfully !"
+   
     data = mcq_utils.read_json(file)
     if request.method=="POST":
         length=len(data["student registration"])
@@ -41,10 +39,40 @@ def reg():
         }
         data["student registration"].append(list_of_stud)
         mcq_utils.write_json(file,data)
-        msg="user " + username +" has been registered successfully!!!"
+        msg= username + " registration has been completed successfully !!"
         data = mcq_utils.read_json(file)
-        return render_template("login.html", data= data["student registration"], msg=msg)
+        return render_template("login.html",data=data["student registration"],msg=msg )
     return render_template("mcq2.html", data=data["student registration"])
+
+@app.route("/dashboard", methods=["GET" , "POST"])
+def dash():
+    data = mcq_utils.read_json(file)
+    if request.method=="POST":
+        email=request.form["Email"]
+        password=request.form["Password"]
+        message="your password or email incorrect"
+        for i in data["student registration"] : 
+            if i["Email"]==email :
+                if i["Password"]==password:
+                    return render_template("dashboard.html", data=data["student registration"], email=email)
+        return render_template("login.html" ,message=message)
+
+@app.route("/caution", methods=["POST", "GET"])
+def caution():
+    return render_template("caution.html")  
+@app.route("/caution2" , methods=["POST", "GET"])  
+def cau():
+    data = mcq_utils.read_json(file)
+    if request.method=="POST":
+        passwrd=request.form["pasword"] 
+        mesage="only owner / admin can open this section" 
+        if passwrd=="rnaveen2520@":
+            return render_template("mcq2.html",  data=data["student registration"]) 
+        else:
+            return render_template("caution.html",mesage=mesage)
+    
+       
+      
     
 if __name__=="__main__":
     app.run(debug=True)   
